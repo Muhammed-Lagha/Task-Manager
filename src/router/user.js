ed7f3c62e833ecedd65cd815942cc530d40dab6a
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-06-02 21:01:19 +0200

    logging in route

--- src/router/user.js
+++ src/router/user.js
@@ -13,6 +13,17 @@ router.post('/users', async (req, res) => {
     res.status(404).send(error)
   }  
 })
+
+// login router
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    res.send(user)
+  } catch (error) {
+    res.status(400).send({ error: 'Invalid login credentials' })
+  }
+})
+
 // find users 
 router.get('/users', async (req, res) => {
   
